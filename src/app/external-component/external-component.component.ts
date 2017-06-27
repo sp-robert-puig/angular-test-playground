5c41c8d29d6f2232e5819ea880e6dc58878bafaa
Robert Puig Bosch <robert.puig@socialpoint.es>
2017-06-27 18:36:50 +0200

    second commit

--- src/app/external-component/external-component.component.ts
+++ src/app/external-component/external-component.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ExternalComponent implements OnInit {
   title = 'external component!'
   constructor() { }
-
+  //test
   ngOnInit() {
-  }
 
+  }
 }
